10c481bf1efad6edb58b862b198ee1cad5197d72
Ankit Meshram <ankitdm69@gmail.com>
2024-06-29 17:04:38 +0530

    Login and register page designed

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/cloudflare";
 import {
   Links,
   Meta,
@@ -5,7 +6,6 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,11 +15,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" ></link> */}
       </head>
       <body>
         {children}
         <ScrollRestoration />
         <Scripts />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
       </body>
     </html>
   );
@@ -28,3 +30,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return <Outlet />;
 }
+
+export const links:LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+  }
+]
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,60 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { Link } from "@remix-run/react";
+import "~/styles/styles.css";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    {
-      name: "description",
-      content: "Welcome to Remix on Cloudflare!",
-    },
-  ];
-};
-
-export default function Index() {
+const _index = () => {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
+    <div className="container-fluid bg-back">
+      <div className="row justify-content-center align-items-center h-100vh">
+        <div className="col-xxl-4 col-xl-5 col-lg-6 col-md-7 col-sm-8">
+          <form className="shadow gp-5 bg-white rounded-lg">
+            <h2 className="text-center fw-bolder mb-2">PCRM</h2>
+            <h4 className="text-center fw-bold mb-2">Welcome Back !</h4>
+            <p className="text-center mb-4">Sign in to continue</p>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {/* <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div> */}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Remember Me
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary w-100">
+              Sign in
+            </button>
+            <p className="text-center mt-4">
+              Don't have an account? <Link to={"/register"}>Register Now</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default _index;
